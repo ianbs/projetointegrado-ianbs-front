98658488caa79bf832c86e75104ef4ec27f73055
Ian B. S <ianbarbosasantos@gmail.com>
2022-03-10 09:55:15 -0300

    fix: Corrigindo importações para build do projeto

--- pages/administracao/permissoes.js
+++ pages/administracao/permissoes.js
@@ -4,47 +4,47 @@ import { useState } from "react";
 import styled from "styled-components";
 // import nookies, { parseCookies } from "nookies";
 
-import HeadPage from "../Components/Head";
-import SidebarComponent from "../Components/SideMenu/SideMenu/Sidebar";
-import HeaderComponent from "../Components/Header";
+import HeadPage from "../../../src/Components/Head";
+import SidebarComponent from "../../../src/Components/SideMenu/Sidebar";
+import HeaderComponent from "../../../src/Components/Header";
 
 export default function Permissoes() {
-  const router = useRouter();
+	const router = useRouter();
 
-  return (
-    <div className="">
-      <HeadPage pageTitle={"[Adminisração]"} />
-      <SidebarComponent
-        ativo={"administracao"}
-        subitem={"faturaexames"}
-      ></SidebarComponent>
-      <Main>
-        <HeaderComponent
-          title={"Financeiro - Faturamento - Exames"}
-        ></HeaderComponent>
-        <MainContent>Faturamento Exames</MainContent>
-      </Main>
-    </div>
-  );
+	return (
+		<div className="">
+			<HeadPage pageTitle={"[Adminisração]"} />
+			<SidebarComponent
+				ativo={"administracao"}
+				subitem={"faturaexames"}
+			></SidebarComponent>
+			<Main>
+				<HeaderComponent
+					title={"Financeiro - Faturamento - Exames"}
+				></HeaderComponent>
+				<MainContent>Faturamento Exames</MainContent>
+			</Main>
+		</div>
+	);
 }
 
 const Main = styled.main`
-  margin-left: 260px;
+	margin-left: 260px;
 `;
 
 const MainContent = styled.main`
-  margin-top: 80px;
-  padding: 2rem 1.5rem;
-  background: var(--white);
-  min-height: calc(100vh - 90px);
-  display: flex;
-  .card {
-    width: 100%;
-  }
-  .list-search {
-    height: 70vh;
-  }
-  .btn-group {
-    width: 100%;
-  }
+	margin-top: 80px;
+	padding: 2rem 1.5rem;
+	background: var(--white);
+	min-height: calc(100vh - 90px);
+	display: flex;
+	.card {
+		width: 100%;
+	}
+	.list-search {
+		height: 70vh;
+	}
+	.btn-group {
+		width: 100%;
+	}
 `;
