797c5d601693ab6c07fafa05a1278d0566095de7
Ian Barbosa Santos <ianbarbosasantos@gmail.com>
2022-09-27 15:56:41 -0300

    feat: change api link

--- services/api.js
+++ services/api.js
@@ -4,7 +4,8 @@ import { parseCookies } from "nookies";
 const { projintegtoken: token } = parseCookies();
 
 export const api = axios.create({
-  baseURL: "https://sgcc-ianbs.herokuapp.com",
+  // baseURL: "https://sgcc-ianbs.herokuapp.com",
+  baseURL: "http://134.122.123.15:3000",
   headers: {
     "Access-Control-Allow-Origin": "*",
   },
